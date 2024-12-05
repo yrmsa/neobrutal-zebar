00d9707cc6592c26451d86db8b640c6ac03f617a
shafiq.affandi <muhammad.sa@ad-ins.com>
2024-12-05 10:33:53 +0700

    feat: battery charging state icon

--- src/components/LeftGroup.svelte
+++ src/components/LeftGroup.svelte
@@ -24,7 +24,22 @@
     <Meter class="bg-zb-cpu" percent={Math.round(cpu?.usage ?? 0)} />
   </div>
   <div class="flex gap-1 items-center">
-    <i class="ti ti-bolt"></i>
+    {#if battery?.isCharging}
+      <i class="ti ti-battery-vertical-charging"></i>
+    {/if}
+    {#if !battery?.isCharging}
+      {#if Math.round(battery?.chargePercent) >= 80}
+        <i class="ti ti-battery-vertical-4"></i>
+      {:else if Math.round(battery?.chargePercent) >= 60}
+        <i class="ti ti-battery-vertical-3"></i>
+      {:else if Math.round(battery?.chargePercent) >= 40}
+        <i class="ti ti-battery-vertical-2"></i>
+      {:else if Math.round(battery?.chargePercent) >= 20}
+        <i class="ti ti-battery-vertical-1"></i>
+      {:else}
+        <i class="ti ti-battery-vertical-exclamation"></i>
+      {/if}
+    {/if}
     <Meter
       class="bg-zb-battery-good"
       percent={Math.round(battery?.chargePercent ?? 100)}
